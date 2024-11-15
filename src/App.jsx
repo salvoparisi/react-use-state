fcd498e913f6fce6c8d2b999aeca1766db9fd304
salvoparisi <psalvo065@gmail.com>
2024-11-15 14:16:18 +0100

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 import React, { useState } from "react";
+import languages from './assets/languages.js'
 export default function App() {
+  const [selectedBtn, setSelectedBtn] = useState(0)
 
+  return (
+    <>
+      <header>
+        <h1>Learn Web Development</h1>
+      </header>
+      <main>
+        <div className="container">
+          {languages.map(obj => {
+            return <button key={obj.id} onClick={() => setSelectedBtn(obj.description)}>{obj.title}</button>
+          })}
+        </div>
+        <div>{selectedBtn ? (selectedBtn) : ("no languages selected")}</div>
+      </main>
+    </>
+  )
 }
